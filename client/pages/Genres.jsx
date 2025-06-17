c1c09b451d573f7d52da9bdbea8f33409fbdddf6
danjhuma <dhanjhuma@gmail.com>
2025-06-17 23:26:17 +0100

    Update Genres.jsx

--- client/pages/Genres.jsx
+++ client/pages/Genres.jsx
@@ -3,7 +3,7 @@ import axios from '../utils/axios';
 
 const Genres = () => { const [genres, setGenres] = useState([]);
 
-useEffect(() => { axios.get('${import.meta.env.VITE_API_BASE_URL}/api/genres') .then(res => setGenres(res.data)) .catch(err => console.error(err)); }, []);
+useEffect(() => { axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/genres`) .then(res => setGenres(res.data)) .catch(err => console.error(err)); }, []);
 
 return ( <div className="genres-page"> <h2>Genre Breakdown</h2> <ul> {genres.map(([genre, count], i) => ( <li key={i}> {genre}: {count} </li> ))} </ul> </div> ); };
 
