fafcd4a8f432a8801c5645b3e7dbc7fde4f3db95
danjhuma <dhanjhuma@gmail.com>
2025-06-17 12:26:01 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
- import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react';
 
 export default defineConfig({ plugins: [react()], server: { proxy: { '/auth': 'http://localhost:5000', '/api': 'http://localhost:5000' } } });
 
