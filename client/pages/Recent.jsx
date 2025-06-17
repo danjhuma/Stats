5d15c193ce360d36cf847321c13a2a0137982e16
danjhuma <dhanjhuma@gmail.com>
2025-06-17 23:22:34 +0100

    Update Recent.jsx

--- client/pages/Recent.jsx
+++ client/pages/Recent.jsx
@@ -3,7 +3,7 @@ import axios from '../utils/axios';
 
 const Recent = () => { const [tracks, setTracks] = useState([]);
 
-useEffect(() => { axios.get('/api/recent') .then(res => setTracks(res.data.items)) .catch(err => console.error(err)); }, []);
+useEffect(() => { axios.get('${import.meta.env.VITE_API_BASE_URL}/api/recent') .then(res => setTracks(res.data.items)) .catch(err => console.error(err)); }, []);
 
 return ( <div className="recent-page"> <h2>Recently Played</h2> <ul> {tracks.map((item, index) => ( <li key={index}> {item.track.name} by {item.track.artists.map(a => a.name).join(', ')} </li> ))} </ul> </div> ); };
 
