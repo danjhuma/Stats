9754dd504e12ced5ad64f1b4837a3ec56a3593cc
danjhuma <dhanjhuma@gmail.com>
2025-06-17 12:38:02 +0100

    Create Genres.jsx

---
+++ client/pages/Genres.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react'; import axios from 'axios';
+
+const Genres = () => { const [genres, setGenres] = useState([]);
+
+useEffect(() => { axios.get('/api/genres') .then(res => setGenres(res.data)) .catch(err => console.error(err)); }, []);
+
+return ( <div className="genres-page"> <h2>Genre Breakdown</h2> <ul> {genres.map(([genre, count], i) => ( <li key={i}> {genre}: {count} </li> ))} </ul> </div> ); };
+
+export default Genres;
+
