b60583e9c9d79fbe9ca2a9777776fd25f434c740
danjhuma <dhanjhuma@gmail.com>
2025-06-18 00:36:21 +0100

    Update App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,22 @@
-
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Callback from './pages/Callback';
+import TopTracks from './pages/TopTracks';
+import TopArtists from './pages/TopArtists';
+import AudioAnalysis from './pages/AudioAnalysis';
 
-const App = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-     <Route path="/callback" element={<Callback />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/callback" element={<Callback />} />
+        <Route path="/top-tracks" element={<TopTracks />} />
+        <Route path="/top-artists" element={<TopArtists />} />
+        <Route path="/audio-analysis" element={<AudioAnalysis />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
