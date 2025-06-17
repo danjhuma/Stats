cd58898c8760deb7600f8f7d55a61df3bc2d91a2
danjhuma <dhanjhuma@gmail.com>
2025-06-17 22:00:11 +0100

    Update Genres.jsx

--- client/pages/Genres.jsx
+++ client/pages/Genres.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'; import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import axios from '../utils/axios';
 
 const Genres = () => { const [genres, setGenres] = useState([]);
 
