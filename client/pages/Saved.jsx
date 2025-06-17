7947650f86aca5d5faa2d12d26187eaf9764db3f
danjhuma <dhanjhuma@gmail.com>
2025-06-17 12:39:06 +0100

    Create Saved.jsx

---
+++ client/pages/Saved.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'; import axios from 'axios';
+
+const Saved = () => { const [tracks, setTracks] = useState([]); const [playlists, setPlaylists] = useState([]);
+
+useEffect(() => { axios.get('/api/saved/tracks') .then(res => setTracks(res.data.items)) .catch(err => console.error(err));
+
+axios.get('/api/saved/playlists')
+  .then(res => setPlaylists(res.data.items))
+  .catch(err => console.error(err));
+
+}, []);
+
+return ( <div className="saved-page"> <h2>Saved Tracks</h2> <ul> {tracks.map((item, i) => ( <li key={i}>{item.track.name} by {item.track.artists.map(a => a.name).join(', ')}</li> ))} </ul>
+
+<h2>Saved Playlists</h2>
+  <ul>
+    {playlists.map((playlist, i) => (
+      <li key={i}>{playlist.name}</li>
+    ))}
+  </ul>
+</div>
+
+); };
+
+export default Saved;
+
