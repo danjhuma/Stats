57c14222c789e91ff50cd6112aa3eb5d53b903e7
danjhuma <dhanjhuma@gmail.com>
2025-06-18 08:52:25 +0100

    Update axios.js

--- client/utils/axios.js
+++ client/utils/axios.js
@@ -1,6 +1,9 @@
-import axios from "axios";
+// utils/axios.js
+import axios from 'axios';
 
-export default axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
+const axiosInstance = axios.create({
+  { baseURL: import.meta.env.VITE_API_URL },
+  withCredentials: true
 });
+
+export default axiosInstance;
